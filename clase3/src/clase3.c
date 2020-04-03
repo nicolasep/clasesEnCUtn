f7b9ed3728ff49df213809ec4e4c8602803eeb1d
nicolasep <nicolaseduardoperez@gmail.com>
2020-04-03 09:44:51 -0300

    ejercicio clase3 terminado

--- clase3/src/clase3.c
+++ clase3/src/clase3.c
@@ -15,8 +15,7 @@
 int sumar(int num1,int num2);
 int restar(int num1, int num2);
 int multiplicar(int num1, int num2);
-float dividir(int num1, int num2);
-
+int dividir(int num1, int num2, float* resultado);
 
 int main(void) {
 	setbuf(stdout,NULL);
@@ -35,6 +34,7 @@ int main(void) {
 	int num2;
 	int resultado1;
 	float resultadoDivision;
+	int flagDivision;
 
 
 	printf("Ingreese un numero: ");
@@ -60,7 +60,7 @@ int main(void) {
 		resultado1 = restar(num1,num2);
 		break;
 	case '3':
-		resultadoDivision = dividir(num1,num2);
+		flagDivision = dividir(num1,num2,&resultadoDivision);
 		break;
 	case '4':
 		resultado1 = multiplicar(num1, num2);
@@ -68,15 +68,17 @@ int main(void) {
 	default:
 		printf("Opcion incorrecta\n");
 	}
+
+
 	if(opcion == '3')
 	{
-		if(resultadoDivision == -1)
+		if(flagDivision)
 		{
-			printf("No see puede dividir por 0\n");
+			printf("El resultado de la operacion es : %.2f",resultadoDivision);
 		}
 		else
 		{
-			printf("El resultado de la operacion es : %.2f",resultadoDivision);
+			printf("No see puede dividir por 0\n");
 		}
 
 	}
@@ -109,19 +111,82 @@ int multiplicar(int num1, int num2)
 
 	return resultado;
 }
-float dividir(int num1, int num2)
-{
-	float resultado;
 
-	if(num1 == 0 || num2 == 0)
+int dividir(int num1, int num2, float* resultado)
+{
+	int flag=0;
+
+		if(num2 == 0)
 		{
-			return -1;
+
 		}
 		else
 		{
-			resultado = (float) num1/num2;
+			*resultado = (float) num1/num2;
+			flag = 1;
 		}
-
-
-	return resultado;
+	return flag;
 }
+
+
+/* EJERCICIO PROFESOR
+ #include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+int dividir(int operador1, int operador2,float* direccionDeVariableDeRetorno);
+
+int main(void)
+{
+	setbuf(stdout,NULL);
+
+	int a=27;
+	int b=2;
+	float resultado;
+	int retHuboError;
+
+	printf("la direccion de resultado en el main es: %d\n",&resultado);
+
+
+	retHuboError = dividir(a,b,&resultado);
+	if(retHuboError==1)
+	{
+		printf("no se puede dividir");
+	}
+	else
+	{
+		printf("resultado:%f",resultado);
+	}
+
+
+
+    return EXIT_SUCCESS;
+}
+
+int dividir(int operador1, int operador2,float* direccionDeVariableDeRetorno)
+{
+	float resul;
+	int huboError;
+
+	printf("la direccion de resultado en funcion dividir es: %d",direccionDeVariableDeRetorno);
+
+	if(operador2!=0)
+	{
+		resul =  (float)operador1 / operador2;
+		// escribir en la direccion direccionDeVariableDeRetorno el resultado de la division "resul"
+		// HACER (ni idea como)
+
+		//escribimeEnLamemoriaEsto(direccionDeVariableDeRetorno,resul);
+		(*direccionDeVariableDeRetorno) = resul;
+
+		huboError=0;//no hubo error
+	}
+	else
+	{
+		huboError=1;//hubo error
+	}
+	return huboError;
+}
+ */
+
