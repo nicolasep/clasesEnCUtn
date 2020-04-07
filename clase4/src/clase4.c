f062d1280f2d0ab88fb6e8e73503bf6ee875b437
nicolasep <nicolaseduardoperez@gmail.com>
2020-04-07 18:23:38 -0300

    clase4

---
+++ clase4/src/clase4.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : clase4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
